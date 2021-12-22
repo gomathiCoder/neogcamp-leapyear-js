56fd2a73938de72b7546fc6eeab1e8b1aa853280
gomathiCoder <91526592+gomathiCoder@users.noreply.github.com>
2021-12-22 20:25:40 +0530

    feat: input validation added and added style to output

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ var username = document.querySelector("#name");
 var dob = document.querySelector("#dob");
 var btnSubmit = document.querySelector("#btn-submit");
 var output = document.querySelector("#output");
+var warningName = document.querySelector("#warning-username");
+var warningDob = document.querySelector("#warning-dob");
+
+function inputValidation(){
+    if(username.value == ""){
+        warningName.style.display = "block";
+    }
+    else if(dob.value == ""){
+        warningName.style.display = "none";
+        warningDob.style.display = "block";
+    }
+    else{
+        warningName.style.display = "none";
+        warningDob.style.display = "none";
+        leapYearCheck();
+    }
+}
 
 function leapYearCheck(){
     let inputYear = dob.value.slice(0,4);
@@ -9,12 +26,14 @@ function leapYearCheck(){
     console.log(inputYear);
     if( (inputYear % 4 === 0 && inputYear % 100 !== 0) || (inputYear % 400 === 0)){
         console.log("Leap year");
-        output.innerText = "Wow " + username.value + "! You have born on leap year!!";
+        output.style.display = "block";
+        output.innerHTML = "Wow " + username.value + "! You have born on <span id='result'>leap year!!</span>";
     }
     else{
-        output.innerText = "Hey " + username.value + "! Your birth year is not a leap year";
+        output.style.display = "block";
+        output.innerHTML = "Hey " + username.value + "! Your birth year is <span id='result'>not a leap year</span>";
         console.log("Not leap year");
     }
 }
 
-btnSubmit.addEventListener("click", leapYearCheck);
+btnSubmit.addEventListener("click", inputValidation);
